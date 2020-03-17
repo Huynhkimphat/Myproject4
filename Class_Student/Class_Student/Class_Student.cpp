ce33249502c939b73334566b38efaa181b44cf90
Huynhkimphat <hkimphat2012@gmail.com>
2020-03-17 16:38:04 +0700

    Update Class_Student.cpp

--- Class_Student/Class_Student/Class_Student.cpp
+++ Class_Student/Class_Student/Class_Student.cpp
@@ -1,32 +1,23 @@
 #include <iostream>
 #include"Class.h"
+#include<vector>
 using namespace std;
 int main()
 {
-    Student *a;
-    while(true)
+    Student* a;
+    a = new Student[100];
+    for (int i = 0; i < 100; i++)
     {
-        a = new Student;
         a->Input();
-        if (a->Grade == 6 || a->Grade == 7)
-            {
-                ((Grade6_7*)a)->Input();
-                ((Grade6_7*)a)->Average();
-                ((Grade6_7*)a)->Output();
-            }
-        if (a->Grade == 8 || a->Grade == 9)
-            {
-                ((Grade8_9*)a)->Input();
-                ((Grade8_9*)a)->Average();
-                ((Grade8_9*)a)->Output();
-            }
-        if (a->Grade == 10 || a->Grade == 11 || a->Grade==12)
+        if (a->Grade == -1)
         {
-            ((Grade10_11_12*)a)->Input();
-            ((Grade10_11_12*)a)->Average();
-            ((Grade10_11_12*)a)->Output();
+            break;
         }
+        float av=Check(a);
+        cout << "Average Score: " << av<<endl;
+        
     }
+    
 
 }
 
