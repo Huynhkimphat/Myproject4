305b139bece1b9f8e0f2dfe58445ab7b9a7e0c39
Huynhkimphat <hkimphat2012@gmail.com>
2020-03-17 16:38:07 +0700

    Update Class.h

--- Class_Student/Class_Student/Class.h
+++ Class_Student/Class_Student/Class.h
@@ -9,47 +9,23 @@ private:
 protected:
 	float fMath, fLit;
 public:
+	float AveScore;
 	int Grade;
-	float AveScore=0;
-	void Input()
+	virtual void Input();
+	virtual float Average()
 	{
-		cout << "Name: ";
-		cin.ignore();
-		getline(cin, s);
-		cout << "Grade: ";
-		cin >> Grade;
-		cout << "Input Math score: ";
-		cin >> fMath;
-		cout << "Input Literature score: ";
-		cin >> fLit;
+		return AveScore = (fMath + fLit) / 2;
 	}
-	void Average()
-	{
-		AveScore = (fMath + fLit) / 2;
-	}
-	
-	
-
 };
 class Grade6_7:public Student
 {
 private:
 	float fGDCD;
 public:
-	void Input()
+	void Input();
+	float Average()
 	{
-		cout << "Input GDCD Score: ";
-		cin >> fGDCD;
-		
-	}
-	void Average()
-	{
-		AveScore = 0;
-		AveScore = (fMath + fLit + fGDCD)/3;
-	}
-	void Output()
-	{
-		cout << "Average Score: " << AveScore << endl;
+		 return AveScore = (fMath + fLit + fGDCD)/3;
 	}
 };
 class Grade8_9 :public Student
@@ -57,22 +33,10 @@ class Grade8_9 :public Student
 private:
 	float fPhy,fChem;
 public:
-	void Input()
+	void Input();
+	float Average()
 	{
-		cout << "Input Physic Score: ";
-		cin >> fPhy;
-		cout << "Input Chemistry Score: ";
-		cin >> fChem;
-
-	}
-	void Average()
-	{
-		AveScore = 0;
-		AveScore = (fMath + fLit + fPhy+fChem) / 4;
-	}
-	void Output()
-	{
-		cout << "Average Score: " << AveScore << endl;
+		return AveScore = (fMath + fLit + fPhy+fChem) / 4;
 	}
 };
 class Grade10_11_12 : public Student
@@ -80,19 +44,11 @@ class Grade10_11_12 : public Student
 private:
 	float fEng;
 public:
-	void Input()
+	void Input();
+	float Average()
 	{
-		cout << "Input English Score: ";
-		cin >> fEng;
-	}
-	void Average()
-	{
-		AveScore = 0;
-		AveScore = (fMath + fLit + fEng) / 3;
-	}
-	void Output()
-	{
-		cout << "Average Score: " << AveScore << endl;
+		return AveScore = (fMath + fLit + fEng) / 3;
 	}
 };
 
+float Check(Student* a);
