f0d9f65cd3b75a8aeba54729e847cbb5ec6abb38
Huynhkimphat <hkimphat2012@gmail.com>
2020-03-18 17:06:20 +0700

    Update Class.cpp

--- Class_Student/Class_Student/Class.cpp
+++ Class_Student/Class_Student/Class.cpp
@@ -2,71 +2,18 @@
 #include"Class.h"
 #include<string>
 using namespace std;
-void Student::Input()
+void Grade6_7::Init(string str, float a,float b,float c)
 {
-	cout << "Name: ";
-	cin.ignore();
-	getline(cin, s);
-	cout << "Grade: ";
-	cin >> Grade;
-
+	s = str;
+	fMath = a; fLit = b; fGDCD = c;
 }
-void Grade6_7::Input()
+void Grade8_9::Init(string str, float a, float b, float c, float d)
 {
-	{
-		cout << "Input Math Score: ";
-		cin >> fMath;
-		cout << "Input Literature Score: ";
-		cin >> fLit;
-		cout << "Input GDCD Score: ";
-		cin >> fGDCD;
-
-	}
+	s = str;
+	fMath = a; fLit = b; fPhy = c; fChem = d;
 }
-void Grade8_9::Input()
+void Grade10_11_12::Init(string str, float a, float b, float c)
 {
-	cout << "Input Math Score: ";
-	cin >> fMath;
-	cout << "Input Literature Score: ";
-	cin >> fLit;
-	cout << "Input Physic Score: ";
-	cin >> fPhy;
-	cout << "Input Chemistry Score: ";
-	cin >> fChem;
-
-}
-void Grade10_11_12::Input()
-{
-	{
-		cout << "Input Math Score: ";
-		cin >> fMath;
-		cout << "Input Literature Score: ";
-		cin >> fLit;
-		cout << "Input English Score: ";
-		cin >> fEng;
-	}
-}
-float Check(Student* a)
-{
-	if (a->Grade == 6 || a->Grade == 7)
-	{
-		a = new Grade6_7;
-		a->Input();
-		float av=a->Average();
-		return av;
-	}
-	if (a->Grade == 8 || a->Grade == 9)
-	{
-		a = new Grade8_9;
-		a->Input();
-		float av = a->Average();
-		return av;
-	}
-	if (a->Grade == 10 || a->Grade == 11||a->Grade==12 )
-	{
-		a = new Grade10_11_12;
-		a->Input();
-		float av = a->Average();
-		return av;
-	}
+	s = str;
+	fMath = a; fLit = b; fEng = c;
 }
