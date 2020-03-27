e1b4e220862f62293f70f2c377072f0d0d188631
Huynhkimphat <hkimphat2012@gmail.com>
2020-03-27 16:40:35 +0700

    Update Class.cpp

--- Class_Student/Class_Student/Class.cpp
+++ Class_Student/Class_Student/Class.cpp
@@ -4,16 +4,36 @@
 using namespace std;
 void Grade6_7::Init(string str, float a,float b,float c)
 {
-	s = str;
+	strName = str;
 	fMath = a; fLit = b; fGDCD = c;
 }
 void Grade8_9::Init(string str, float a, float b, float c, float d)
 {
-	s = str;
+	strName = str;
 	fMath = a; fLit = b; fPhy = c; fChem = d;
 }
 void Grade10_11_12::Init(string str, float a, float b, float c)
 {
-	s = str;
+	strName = str;
 	fMath = a; fLit = b; fEng = c;
+}
+string Student::to_string()
+{
+	stringstream ss;
+	if (Grade == 6 || Grade == 7)
+	{
+		ss << "Name: " << strName << ", Grade: " << Grade << ", Math: " << fMath << ", Literater: " << fLit << ", GDCD: " << fGDCD<<", Ave: "<<AveScore;
+		return ss.str();
+	}
+	if (Grade == 9 || Grade == 8)
+	{
+		stringstream ss;
+		ss << "Name: " << strName << ", Grade: " << Grade << ", Math: " << fMath << ", Literater: " << fLit << ", Physic: " << fPhy << ", Chemistry: " << fChem << ", Ave: " << AveScore;
+		return ss.str();
+	}
+	if (Grade == 10 || Grade == 11 || Grade == 12)
+	{
+		ss << "Name: " << strName << ", Grade: " << Grade << ", Math: " << fMath << ", Literater: " << fLit << ", English: " << fEng << ", Ave: " << AveScore;
+		return ss.str();
+	}
 }
